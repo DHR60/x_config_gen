6b9de8a52c5b93f4ee63252f8169d10c4a4a9999
DHR60 <dehongren60@gmail.com>
2025-12-30 18:57:00 +0800

    fix. import path

--- lib/src/xray/transport/network/xhttp.dart
+++ lib/src/xray/transport/network/xhttp.dart
@@ -1,6 +1,6 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:x_config_gen/src/core/multi_value/multi_value_string.dart';
-import 'package:x_config_gen/src/xray/inbounds/inbound.dart';
+import 'package:x_config_gen/src/xray/transport/security/reality.dart';
 import 'package:x_config_gen/src/xray/transport/security/tls.dart';
 import 'package:x_config_gen/src/xray/transport/sockopt/sockopt.dart';
 
